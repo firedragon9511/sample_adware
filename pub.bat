da1043176d88d184a2b22186fbd224e5bf2cf6d1
firedragon <firedragon9511@gmail.com>
2022-10-01 12:17:22 -0300

    first commit

--- pub.bat
+++ pub.bat
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "first commit"
+git commit -m "first commit" 
 git branch -M main
-git remote add origin --force https://github.com/firedragon9511/sample_adware.git
-git push -u origin main
+git remote add origin https://github.com/firedragon9511/sample_adware.git
+git push -u origin main --force
